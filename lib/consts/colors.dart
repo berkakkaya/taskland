1e533fd154ef2452a859006e827b23eb9ca38214
Berk Akkaya <berkakkaya000@gmail.com>
2024-03-08 11:14:00 +0300

    add: color constants

---
+++ lib/consts/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const colorPrimary = Color(0xFF525A92);
+
+const colorOrange = Color(0xFFEEA57A);
+const colorRed = Color(0xFFD97979);
+const colorGreen = Color(0xFFB4DF57);
+const colorBlue = Color(0xFF6BC5E1);
+const colorPurple = Color(0xFFAC85FF);
