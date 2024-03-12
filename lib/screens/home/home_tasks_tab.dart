15901bf54a3de8dcebe71cfdb97b66df7afb90e3
Berk Akkaya <berkakkaya000@gmail.com>
2024-03-13 01:17:11 +0300

    feat: make listview ready for listing items

--- lib/screens/home/home_tasks_tab.dart
+++ lib/screens/home/home_tasks_tab.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:taskland/consts/illustrations.dart';
+import 'package:taskland/services/storage.dart';
 
 class HomeTasksTab extends StatefulWidget {
   const HomeTasksTab({super.key});
@@ -10,8 +13,49 @@ class HomeTasksTab extends StatefulWidget {
 class _HomeTasksTabState extends State<HomeTasksTab> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Tasks"),
+    return ValueListenableBuilder(
+      valueListenable: StorageService.tasksBox.listenable(),
+      builder: (context, box, widget) {
+        if (box.isEmpty) {
+          return _getEmptyPage(context);
+        }
+
+        return ListView.separated(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+          separatorBuilder: (context, index) => const SizedBox(height: 24),
+          itemCount: box.length,
+          itemBuilder: (context, index) {
+            return const Placeholder();
+          },
+        );
+      },
+    );
+  }
+
+  Padding _getEmptyPage(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Spacer(),
+          Expanded(
+            child: svgNature,
+          ),
+          const SizedBox(height: 32),
+          Text(
+            "Listeniz boş",
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "Hemen sağ alttaki butondan yeni görevler eklemeye başlayın.",
+            textAlign: TextAlign.center,
+          ),
+          const Spacer(),
+        ],
+      ),
     );
   }
 }
