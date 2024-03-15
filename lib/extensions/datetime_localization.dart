6ba6a9dac868209d737f70c89d71f77be79ecd0e
Berk Akkaya <berkakkaya000@gmail.com>
2024-03-16 00:06:47 +0300

    feat: add localized date and time generator extensions

---
+++ lib/extensions/datetime_localization.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+extension DateTimeLocalization on DateTime {
+  String localizedDate(BuildContext context) {
+    String locale = Localizations.localeOf(context).languageCode;
+
+    final String day = DateFormat.d(locale).format(this);
+    final String month = DateFormat.MMMM(locale).format(this);
+    final String year = DateFormat.y(locale).format(this);
+
+    return "$day $month $year";
+  }
+
+  String localizedTime(BuildContext context) {
+    String locale = Localizations.localeOf(context).languageCode;
+
+    final String hour = DateFormat.H(locale).format(this);
+    final String minute = DateFormat.m(locale).format(this);
+
+    return "$hour:$minute";
+  }
+}
