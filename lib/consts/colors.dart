c06006cde8060db46816302ffc87f118510e94d1
Berk Akkaya <berkakkaya000@gmail.com>
2024-03-16 02:49:52 +0300

    feat: add two colors to color constants
    Some colors somewhat doesn't get included in the default Flutter color palette.
    So, for now I added them as a constant, temporarily.

--- lib/consts/colors.dart
+++ lib/consts/colors.dart
@@ -7,3 +7,9 @@ const colorRed = Color(0xFFD97979);
 const colorGreen = Color(0xFFB4DF57);
 const colorBlue = Color(0xFF6BC5E1);
 const colorPurple = Color(0xFFAC85FF);
+
+// These are added because Flutter doesn't provide full Material3 palette yet.
+// TODO: Remove these when these colors are fully implemented.
+
+const m3OnSurfaceVariant = Color(0xFF46464F);
+const m3SurfaceContainerHigh = Color(0xFFE9E7EF);
