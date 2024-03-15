ee2b0881f2dc364d81fa10732abb944316094bf5
Berk Akkaya <berkakkaya000@gmail.com>
2024-03-16 02:48:07 +0300

    intl: enable localizations from `flutter_localizations`

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:taskland/consts/colors.dart';
 import 'package:taskland/consts/font_theme.dart';
 import 'package:taskland/screens/introduction/welcome_screen.dart';
 import 'package:taskland/services/storage.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   await StorageService.initialize();
@@ -22,6 +23,16 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Taskland',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('tr'),
+      ],
+      locale: const Locale('tr'),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: colorPrimary),
         useMaterial3: true,
