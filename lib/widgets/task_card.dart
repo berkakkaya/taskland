9e0decd1fed6c67f7fb8c3d8bf43303300dc0cc1
Berk Akkaya <berkakkaya000@gmail.com>
2024-03-18 00:31:17 +0300

    feat: make task delete after got checked for 3 seconds

--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:taskland/extensions/datetime_localization.dart';
 import 'package:taskland/types/task.dart';
@@ -18,17 +20,25 @@ class TaskCard extends StatefulWidget {
 class _TaskCardState extends State<TaskCard> {
   bool? checked = false;
 
+  Timer? deleteTimer;
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
         future: widget.task,
         builder: (context, snapshot) {
+          final hasExtraData = snapshot.data?.date != null ||
+              snapshot.data?.notification != null ||
+              snapshot.data?.importance != null;
+
           return Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Checkbox(
                 value: checked,
-                onChanged: changeChecked,
+                onChanged: snapshot.hasData
+                    ? (val) => changeChecked(snapshot.data!, val)
+                    : null,
               ),
               const SizedBox(width: 8),
               Expanded(
@@ -41,39 +51,41 @@ class _TaskCardState extends State<TaskCard> {
                       overflow: TextOverflow.fade,
                       softWrap: false,
                     ),
-                    const SizedBox(height: 8),
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        if (snapshot.data?.date != null) ...[
-                          const Icon(
-                            Icons.calendar_month_outlined,
-                            size: 16,
-                          ),
-                          const SizedBox(width: 4),
-                          Text(snapshot.data!.date!.localizedDate(context)),
-                          const SizedBox(width: 12),
+                    if (hasExtraData) ...[
+                      const SizedBox(height: 8),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          if (snapshot.data?.date != null) ...[
+                            const Icon(
+                              Icons.calendar_month_outlined,
+                              size: 16,
+                            ),
+                            const SizedBox(width: 4),
+                            Text(snapshot.data!.date!.localizedDate(context)),
+                            const SizedBox(width: 12),
+                          ],
+                          if (snapshot.data?.notification != null) ...[
+                            const Icon(
+                              Icons.alarm_rounded,
+                              size: 16,
+                            ),
+                            const SizedBox(width: 4),
+                            Text(snapshot.data!.notification!.toString()),
+                            const SizedBox(width: 12),
+                          ],
+                          if (snapshot.data?.importance != null) ...[
+                            const Icon(
+                              Icons.flag_outlined,
+                              size: 16,
+                            ),
+                            const SizedBox(width: 4),
+                            Text(getImportanceText(snapshot.data!.importance!)),
+                          ],
                         ],
-                        if (snapshot.data?.notification != null) ...[
-                          const Icon(
-                            Icons.alarm_rounded,
-                            size: 16,
-                          ),
-                          const SizedBox(width: 4),
-                          Text(snapshot.data!.notification!.toString()),
-                          const SizedBox(width: 12),
-                        ],
-                        if (snapshot.data?.importance != null) ...[
-                          const Icon(
-                            Icons.flag_outlined,
-                            size: 16,
-                          ),
-                          const SizedBox(width: 4),
-                          Text(getImportanceText(snapshot.data!.importance!)),
-                        ],
-                      ],
-                    ),
+                      ),
+                    ],
                   ],
                 ),
               ),
@@ -91,9 +103,25 @@ class _TaskCardState extends State<TaskCard> {
     };
   }
 
-  void changeChecked(bool? newVal) {
+  void changeChecked(Task task, bool? isChecked) {
     setState(() {
-      checked = newVal;
+      checked = isChecked;
     });
+
+    if (isChecked == true) {
+      deleteTimer = Timer(
+        const Duration(seconds: 3),
+        () => task.delete(),
+      );
+
+      return;
+    }
+
+    if (isChecked == false) {
+      deleteTimer?.cancel();
+
+      deleteTimer = null;
+      return;
+    }
   }
 }
